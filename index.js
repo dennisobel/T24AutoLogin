8656a3506fdc2e97c3d4253fbbf62c48a0eb4820
Dennis Obel <ogembodenis2016@gmail.com>
2019-01-23 10:46:52 +0300

    error handle

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+(function() {
+    var childProcess = require("child_process");
+    var oldSpawn = childProcess.spawn;
+    function mySpawn() {
+        console.log('spawn called');
+        console.log(arguments);
+        var result = oldSpawn.apply(this, arguments);
+        return result;
+    }
+    childProcess.spawn = mySpawn;
+})();
+
 const express = require('express');
 const app = express();
 const script = require('./script.js')
